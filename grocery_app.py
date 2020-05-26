ebda02be5144c3cbadd38dc24ccd56ef13b78206
dave00dave <ochs.dave@gmail.com>
2020-05-25 20:38:59 -0500

    Multi-column item presentation

--- grocery_app.py
+++ grocery_app.py
@@ -5,17 +5,18 @@ Created on Sun May 24 20:17:24 2020
 
 @author: daveandmarinda
 """
-
+import numpy as np
 from guizero import App, Box, Text, TextBox, PushButton
 from collections import OrderedDict
 
 class item:
-    def __init__(self, box, order, label, def_val):
-        self.text = Text(box, grid=[0, order], text=label, align="left")
-        self.plus = PushButton(box, grid=[1, order], text="+",
+    def __init__(self, box, row, col, label, def_val):
+        self.text = Text(box, grid=[col+0, row], text=label, align="right")
+        self.plus = PushButton(box, grid=[col+1, row], text="+",
                                command=self.add_1, align="left")
-        self.val = TextBox(box, grid=[2, order], text=str(def_val), align="left")
-        self.minus = PushButton(box, grid=[3, order], text="-",
+        self.val = TextBox(box, grid=[col+2, row], text=str(def_val), width=2,
+                           align="left")
+        self.minus = PushButton(box, grid=[col+3, row], text="-",
                                 command=self.sub_1, align="left")
     def add_1(self):
         val = int(self.val.value)
@@ -41,35 +42,42 @@ options_box = Box(app, height="fill", align="right", border=True)
 Text(options_box, text="options")
 
 content_box = Box(app, align="top", layout="grid", width="fill", border=True)
-Text(content_box, grid=[0,0], text="content")
 
-# form_box = Box(content_box, layout="grid", width="fill", align="left", border=True)
-# Text(form_box, grid=[0,0], text="form", align="right")
-# Text(form_box, grid=[0,1], text="label", align="left")
-# TextBox(form_box, grid=[1,1], text="data", width="fill")
+items = ['fruit',
+         'lettuce',
+         'carrots',
+         'bagels',
+         'bread',
+         'buns',
+         'raisins',
+         'crackers',
+         'graham crackers',
+         'tortilla chips',
+         'mild salsa',
+         'medium salsa',
+         'hummus',
+         'cheese sticks',
+         'pretzels',
+         'popcorn',
+         'flour',
+         'sugar',
+         'brown sugar',
+         'milk',
+         'frozen pizza']
 
-# def add_1():
-#     val = int(bread_val.value)
-#     bread_val.value = str(val+1)
-#
-# def sub_1():
-#     val = int(bread_val.value)
-#     bread_val.value = str(val-1)
-
-items = ['bread', 'milk', 'pizza']
-
-# boxes = []
-# boxes.append(Box(content_box, layout="grid", width="fill", align="left", border=True))
-# Text(bread_box, grid=[0,0], text="Bread", align="left")
-# plus = PushButton(bread_box, grid=[1,0], text="+", command=add_1, align="left")
-# bread_val = TextBox(bread_box, grid=[2,0], text="0", align="left")
-# minus = PushButton(bread_box, grid=[3,0], text="-", command=sub_1, align="left")
 
 item_d = OrderedDict()
-n=1
+r = 0
+c = 0
+c_cnt = 0
 for i in items:
-    item_d.update({i: item(content_box, n, i, 0)})
-    n+=1
+    item_d.update({i: item(content_box, r, c, i, 0)})
+    r += 1
+    c_cnt += 1
+    if np.mod(c_cnt, 10) == 0:
+        c += 4
+        c_cnt = 0
+        r = 0
 
 
 app.display()
