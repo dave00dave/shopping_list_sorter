1dc17552db70932fadcd754119d681335243c674
dave00dave <ochs.dave@gmail.com>
2021-05-29 05:19:19 -0500

    Add save and load cfg tests

--- test_list_sorter.py
+++ test_list_sorter.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from list_sorter import *
 
 # Define fixtures to provide mock inputs to test
@@ -29,6 +30,25 @@ def test_check_no_item_wo_numbers(store_items):
     we get the item back without the (#) at the end.
     """
 
-    i = ' '
+    i = 'no item'
     res = check_without_number(i, store_items)
     assert(res == '')
+
+def test_save_load_cfg():
+    """Test saving and loading a configuration file
+    
+    Combine saving and loading into one test so there's
+    no dependency on having a config file already created
+    """
+
+    test_cfg_fname = 'unit_test.pkl'
+    test_cfg_key = 'foo'
+    test_cfg_val = 'bar'
+    save_cfg_item(test_cfg_key, test_cfg_val, test_cfg_fname)
+
+    loaded_cfg_val = load_cfg_item(test_cfg_key, test_cfg_fname)
+    assert(loaded_cfg_val == test_cfg_val)
+
+    # Remove the test config file
+    if os.path.exists(test_cfg_fname):
+        os.remove(test_cfg_fname)
